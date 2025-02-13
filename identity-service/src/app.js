c13ba0384c3af95a31c12a8bd2340ba26b57e5c3
hungntth <1119thaihungnguyen@gmail.com>
2025-02-13 22:20:15 +0700

    base identity

---
+++ identity-service/src/app.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const helmet = require("helmet");
+const cors = require("cors");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const routes = require("./routes/indentity-service");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
+      res.status(429).json({ sucess: false, mesaage: "Too many request" });
+    });
+});
+
+const sensitiveEndPointsLimiter = rateLimit({
+  windowMs: 15 * 50 * 1000, // 15 phút
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({ success: false, message: "too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use("/api/auth/register", sensitiveEndPointsLimiter);
+app.use("/api/auth", routes);
+app.use(errorHandler);
+
+module.exports = app;
---
+++ identity-service/src/models/RefreshToken.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const refreshTokenSchema = new Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+refreshTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+const RefreshToken = model("RefreshToken", refreshTokenSchema);
+
+module.exports = RefreshToken;
